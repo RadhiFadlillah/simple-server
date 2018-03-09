71b1b985caf668d20b6bd389532dbf2b3b31a33c
Radhi Fadlillah <m.radhi.f@gmail.com>
2018-03-09 11:12:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple-Server
+
+`simple-server` is a simple app to run HTTP server which serve content of the specified directory. I created it because I started working as front-end developer, so I need a simple server to check my works, which easy to install, easy to use, lightweight and have no dependency.
+
+## Installation
+
+Make sure `go` is installed, then run :
+
+```
+go get github.com/RadhiFadlillah/simple-server
+```
+
+## Usage
+
+```
+Run a simple HTTP server to serve content of specified directory
+
+Usage:
+  simple-server directory [flags]
+
+Flags:
+  -h, --help       help for simple-server
+  -p, --port int   Port that used by server (default 8080)
+```
+
+## License
+
+This app is distributed using [MIT license](https://choosealicense.com/licenses/mit/), which means you can use and modify it however you want.
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	fp "path/filepath"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	root := &cobra.Command{
+		Use:   "simple-server directory",
+		Short: "Run a simple HTTP server to serve content of specified directory",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			// Get root directory
+			rootDir := args[0]
+
+			// Create router
+			router := httprouter.New()
+			router.GET("/*filename", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+				filename := ps.ByName("filename")
+				filename = fp.Join(rootDir, filename)
+				http.ServeFile(w, r, filename)
+			})
+
+			router.PanicHandler = func(w http.ResponseWriter, r *http.Request, arg interface{}) {
+				http.Error(w, fmt.Sprint(arg), 500)
+			}
+
+			// Serve app
+			port, _ := cmd.Flags().GetInt("port")
+			addr := fmt.Sprintf(":%d", port)
+			log.Fatalln(http.ListenAndServe(addr, router))
+		},
+	}
+
+	root.Flags().IntP("port", "p", 8080, "Port that used by server")
+	root.Execute()
+}
